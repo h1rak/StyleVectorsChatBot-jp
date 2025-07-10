b61c9d25094d75b6dac64daf6e8967fe6d678b38
h1rak <hiraku.gondo@gmail.com>
2025-07-10 12:23:42 +0000

    add

---
+++ scripts/generation/steering.py
@@ -0,0 +1,40 @@
+import os 
+import pickle
+import torch
+import glob
+from tqdm import tqdm
+import random
+from dotenv import load_dotenv
+from utils.llm_model_utils import load_llm_model_with_insertions
+from steering_helper import interactive_eval, calculate_means
+#from utils.steering_vector_loader import load_activations
+
+
+load_dotenv()
+
+def load_activations(vector_path):
+    with open(f'{vector_path}/activations_train.pkl','rb') as f:
+        train = pickle.load(f)
+
+    with open(f'{vector_path}/activations_test.pkl','rb') as f:
+        test = pickle.load(f)
+
+    return train,test
+
+
+DEVICE = torch.device("cuda:0")
+INSERTION_LAYERS = [18,19,20]
+model,tokenizer = load_llm_model_with_insertions(DEVICE,INSERTION_LAYERS)
+
+ACTIVATION_VECTOR_PATH = os.getenv("ACTIVATIONS_PATH")
+SAVE_PATH = os.getcwd()
+train,test = load_activations(ACTIVATION_VECTOR_PATH)
+
+train = [entry for entry in train if len(entry)==3]
+test = [entry for entry in test if len(entry)==3]
+
+emotion_names = ['Joy', 'Sadness', 'Anticipation', 'Surprise', 'Anger', 'Fear', 'Disgust', 'Trust']
+emotion_idx = [0,1,2,3,4,5,6,7]
+
+means, ovr_r_means, total_mean = calculate_means(train, test, emotion_idx, INSERTION_LAYERS)
+interactive_eval(emotion_names, means, ovr_r_means, total_mean, llm_model=model, tokenizer=tokenizer, insertion_layers=INSERTION_LAYERS, save_path=SAVE_PATH, device=DEVICE)
---
+++ scripts/training/get_hidden_activations.py
@@ -0,0 +1,104 @@
+import os
+import torch
+import transformers
+from tqdm import tqdm
+import pickle
+from dotenv import load_dotenv
+from pathlib import Path
+import pandas as pd
+from transformers import BitsAndBytesConfig
+import numpy as np
+
+df = pd.read_table("/home/santa/chatbot/data/wrime-ver1.tsv")
+df.columns = df.columns.str.strip()
+#emotion_names = ['Joy', 'Sadness', 'Anticipation', 'Surprise', 'Anger', 'Fear', 'Disgust', 'Trust' ]
+#df['readers_emotion_intensities'] = df.apply(lambda x: [x['Avg. Readers_' + name] for name in emotion_names], axis=1)
+#is_target = df['readers_emotion_intensities'].map(lambda x: max(x) >= 2)
+#df = df[is_target]
+
+def wrime_get_single_emo(df, max_only=True, threshold=2):
+    emotion_names = ['Joy', 'Sadness', 'Anticipation', 'Surprise', 'Anger', 'Fear', 'Disgust', 'Trust']
+    selected = []
+    for idx, row in df.iterrows():
+        scores = [row['Avg. Readers_' + name] for name in emotion_names]
+        if max_only:
+            max_idx = int(np.argmax(scores))
+            if scores[max_idx] == 0:
+                continue
+            selected.append({"text": row["Sentence"], "labels": [max_idx], "id": idx, "emo": emotion_names[max_idx],"Train/Dev/Test":row["Train/Dev/Test"]})
+        else:
+            pos = [i for i, s in enumerate(scores) if s >= threshold]
+            if len(pos) == 1:
+                selected.append({"text": row["Sentence"], "labels": [max_idx], "id": idx, "emo": emotion_names[max_idx],"Train/Dev/Test":row["Train/Dev/Test"]})
+    return pd.DataFrame(selected)
+
+
+df = wrime_get_single_emo(df)
+load_dotenv()
+
+MODEL_PATH = os.getenv("MODEL_WEIGHTS_FOLDER")
+
+PATH_TO_ACTIVATION_STORAGE = os.getenv("ACTIVATIONS_PATH")
+Path(PATH_TO_ACTIVATION_STORAGE).mkdir(parents=True,exist_ok=True)
+
+DEVICE = torch.device("cuda:0")
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4"
+)
+
+# 元のコードを以下に置き換え
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    MODEL_PATH,
+    trust_remote_code=True,
+    quantization_config=bnb_config,
+    device_map="auto",  # .to(DEVICE)の代わりに使用
+    torch_dtype=torch.float16
+)
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_PATH)
+model.to(DEVICE)
+
+def process_dataset(df):
+    df = df
+    actis,actis_train,actis_test = [],[],[]
+    i = 0
+    j = 0
+    for index,row in tqdm(df.iterrows(),total=df.shape[0]):
+        sentence = []
+        sentence = row["text"].replace("\n","")
+        input_tokens = tokenizer(sentence,return_tensors="pt").to(DEVICE)
+
+        if len(input_tokens.input_ids) > 300:
+            continue
+        gen_text = model.forward(input_tokens.input_ids,output_hidden_states=True,return_dict=True)
+        hidden_states = []
+
+        for layer in gen_text["hidden_states"]:
+            hidden_states.append(layer[0][-1].detach().cpu().numpy())
+        if row["Train/Dev/Test"] == "train":
+            actis_train.append([index,row,hidden_states])
+        elif row["Train/Dev/Test"] == "test":
+            actis_test.append([index,row,hidden_states])
+
+        i += 1
+        # save activations in batches
+        if i == 6000:
+            i = 0
+            with open(f'{PATH_TO_ACTIVATION_STORAGE}/activations_{j}.pkl', 'wb') as f:
+                pickle.dump(actis, f)
+            del actis
+            del hidden_states
+            actis = []
+            j += 1
+    with open(f'{PATH_TO_ACTIVATION_STORAGE}/activations_train.pkl', 'wb') as f:
+        pickle.dump(actis_train, f)
+    with open(f'{PATH_TO_ACTIVATION_STORAGE}/activations_test.pkl', 'wb') as f:
+        pickle.dump(actis_test, f)
+
+    del actis
+    del hidden_states
+
+process_dataset(df)
